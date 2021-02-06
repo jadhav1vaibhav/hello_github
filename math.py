da31df743336517739f172701088172f90c12f0c
VaibhavJadhav <jadhav1vaibhav@gmail.com>
2021-02-06 18:25:58 +0530

    math.py added

---
+++ math.py
@@ -0,0 +1,16 @@
+# Perform addition
+def add(x,y):
+    return x+y
+# Perform subtraction ex.
+def subtract(x,y):
+    if(x<y):
+        return error
+    return x-y
+# Perform multiplication
+def multiply(x,y):
+    return x*y    
+# Perform division
+def divide(x,y):
+    if(y==0):
+        return Divide_by_0_error
+    return x/y
